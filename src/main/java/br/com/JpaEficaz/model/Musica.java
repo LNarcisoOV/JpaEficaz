a8e3fc6acd0b623cc30b86f22da5adda9ad0d580
Leonardo Narciso <lnarcisoov@gmail.com>
2017-05-31 10:20:55 -0300

    [ANOTACOES] Adicionando mais anotações na entidade Musica.

--- src/main/java/br/com/JpaEficaz/model/Musica.java
+++ src/main/java/br/com/JpaEficaz/model/Musica.java
@@ -3,6 +3,8 @@ package br.com.JpaEficaz.model;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -23,11 +25,23 @@ import javax.persistence.UniqueConstraint;
 	)
 public class Musica {
 	
+	public static final String SEQUENCE_NAME = "SEQUENCIA_MUSICA";
+	
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+					generator = SEQUENCE_NAME)
 	private int id;
 	
 	@Basic(optional = false)
-	@Column(name = "NM_MUSICA", length = 100, unique = true)
+	@Column(name = "NM_MUSICA", 
+			length = 100, 
+			unique = true,
+			nullable = false,
+			columnDefinition = "VARCHAR(45)",
+			insertable = true,
+			updatable = true,
+			precision = 2,
+			scale = 2)
 	private String nome;
 	
 	@ManyToOne
