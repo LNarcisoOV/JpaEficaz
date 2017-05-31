572a28929fd49e77ec32ffefc79c6403f7cbaaeb
Leonardo Narciso <lnarcisoov@gmail.com>
2017-05-31 14:14:06 -0300

    [ANOTACOES] Adicionando tabelas para campos NOME, pois cada tabela pode ter seu campo.

--- src/main/java/br/com/JpaEficaz/model/MusicaVariasTabelas.java
+++ src/main/java/br/com/JpaEficaz/model/MusicaVariasTabelas.java
@@ -33,8 +33,30 @@ public class MusicaVariasTabelas {
 	private Pessoa autor;
 
 	@Basic(optional = false)
-	@Column(name = "NM_MUSICA", length = 100, unique = true, nullable = false, columnDefinition = "VARCHAR(45)", insertable = true, updatable = true, precision = 2, scale = 2)
+	@Column(table = "TB_MUSICA", 
+			name = "NM_MUSICA", 
+			length = 100, 
+			unique = true, 
+			nullable = false, 
+			columnDefinition = "VARCHAR(45)", 
+			insertable = true, 
+			updatable = true, 
+			precision = 2, 
+			scale = 2)
 	private String nome;
+	
+	@Column(table = "MUSICA_2372", 
+			name = "NM_MUSICA", 
+			length = 100, 
+			unique = true, 
+			nullable = false, 
+			columnDefinition = "VARCHAR(45)", 
+			insertable = true, 
+			updatable = true, 
+			precision = 2, 
+			scale = 2)
+	private String nome2;
+
 
 	@ManyToOne
 	private Album album;
@@ -46,4 +68,62 @@ public class MusicaVariasTabelas {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	/**
+	 * @return the codigoUnico
+	 */
+	public CodigoUnico getCodigoUnico() {
+		return codigoUnico;
+	}
+
+	/**
+	 * @param codigoUnico the codigoUnico to set
+	 */
+	public void setCodigoUnico(CodigoUnico codigoUnico) {
+		this.codigoUnico = codigoUnico;
+	}
+
+	/**
+	 * @return the autor
+	 */
+	public Pessoa getAutor() {
+		return autor;
+	}
+
+	/**
+	 * @param autor the autor to set
+	 */
+	public void setAutor(Pessoa autor) {
+		this.autor = autor;
+	}
+
+	/**
+	 * @return the nome2
+	 */
+	public String getNome2() {
+		return nome2;
+	}
+
+	/**
+	 * @param nome2 the nome2 to set
+	 */
+	public void setNome2(String nome2) {
+		this.nome2 = nome2;
+	}
+
+	/**
+	 * @return the album
+	 */
+	public Album getAlbum() {
+		return album;
+	}
+
+	/**
+	 * @param album the album to set
+	 */
+	public void setAlbum(Album album) {
+		this.album = album;
+	}
+	
+	
 }
